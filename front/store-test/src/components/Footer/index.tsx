551bf170dd0ec7f8b0618c313bd2e5a9d6ab91f3
Sebastian Sandoval <sebassandoval@hotmail.com>
2023-04-16 21:11:39 -0500

    Remove users of DB and Update Readme

--- front/store-test/src/components/Footer/index.tsx
+++ front/store-test/src/components/Footer/index.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Footer: React.FC = () => {
   return (
-    <header className='w-full h-16 bg-[#212121] flex justify-center items-center text-white px-7 fixed bottom-0 left-0'>
+    <header className="w-full h-16 bg-[#212121] flex justify-center items-center text-white px-7 fixed bottom-0 left-0">
       <div className="content w-10/12 flex justify-center items-center">
-        <div className='w-1/4 h-full flex items-center justify-center '>
-          <span className='text-lg text-white font-bold'>Logo</span>
+        <div className="w-1/4 h-full flex items-center justify-center ">
+          <span className="text-lg text-white font-bold">Made By Sebastian Sandoval 🚀</span>
         </div>
       </div>
     </header>
